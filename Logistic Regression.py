d3073aea8e3f58eab78d6b17cee4416f2f73a06b
jex <72693376+ministove3yo@users.noreply.github.com>
2020-12-31 15:13:46 +0800

    Add files via upload

---
+++ Logistic Regression.py
@@ -0,0 +1,123 @@
+import numpy as np
+import matplotlib.pyplot as plt
+x_data = np.array([[2, 4], [4, 11], [6, 6], [8, 5], [10, 7], [12, 16], [14, 8], [16, 3], [18, 7]])
+t_data = np.array([0, 0, 0, 0, 1, 1, 1, 1, 1]).reshape(9, 1)
+
+print("x_data.shape = ", x_data.shape, ", t_data.shape = ", t_data.shape)
+
+W = np.random.rand(2,1)
+b = np.random.rand(1)
+print("W = ", W, ", W.shape = ", W.shape, ", b = ", b, ", b.shape = ", b.shape)
+
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+
+def loss_func(x, t):
+    delta = 1e-7  # log 무한대 발산 방지
+
+    z = np.dot(x, W) + b
+    y = sigmoid(z)
+
+    # cross-entropy
+    return -np.sum(t * np.log(y + delta) + (1 - t) * np.log((1 - y) + delta))
+
+
+def numerical_derivative(f, x):
+    delta_x = 1e-4  # 0.0001
+    grad = np.zeros_like(x)
+
+    it = np.nditer(x, flags=['multi_index'], op_flags=['readwrite'])
+
+    while not it.finished:
+        idx = it.multi_index
+        tmp_val = x[idx]
+        x[idx] = float(tmp_val) + delta_x
+        fx1 = f(x)  # f(x+delta_x)
+
+        x[idx] = tmp_val - delta_x
+        fx2 = f(x)  # f(x-delta_x)
+        grad[idx] = (fx1 - fx2) / (2 * delta_x)
+
+        x[idx] = tmp_val
+        it.iternext()
+
+    return grad
+
+
+# 손실함수 값 계산 함수
+# 입력변수 x, t : numpy type
+def error_val(x, t):
+    delta = 1e-7  # log 무한대 발산 방지
+
+    z = np.dot(x, W) + b
+    y = sigmoid(z)
+
+    # cross-entropy
+    return -np.sum(t * np.log(y + delta) + (1 - t) * np.log((1 - y) + delta))
+
+
+# 학습을 마친 후, 임의의 데이터에 대해 미래 값 예측 함수
+# 입력변수 x : numpy type
+def predict(x):
+    z = np.dot(x, W) + b
+    y = sigmoid(z)
+
+    if y > 0.5:
+        result = 1  # True
+    else:
+        result = 0  # False
+
+    return y, result
+
+
+learning_rate = 1e-2  # 발산하는 경우, 1e-3 ~ 1e-6 등으로 바꾸어서 실행
+
+f = lambda x: loss_func(x_data, t_data)  # f(x) = loss_func(x_data, t_data)
+
+print("Initial error value = ", error_val(x_data, t_data), "Initial W = ", W, "\n", ", b = ", b)
+
+for step in range(80001):
+
+    W -= learning_rate * numerical_derivative(f, W)
+
+    b -= learning_rate * numerical_derivative(f, b)
+
+    if (step % 400 == 0):
+        print("step = ", step, "error value = ", error_val(x_data, t_data), "W = ", W, ", b = ", b)
+
+
+test_data = np.array([3, 17]) # (예습, 복습) = (3, 17) => Fail (0)
+
+print(predict(test_data))
+
+test_data = np.array([5, 8]) # (예습, 복습) = (5, 8) => Fail (0)
+
+print(predict(test_data))
+
+test_data = np.array([7, 21]) # (예습, 복습) = (7, 21) => Pass (1)
+
+print(predict(test_data))
+
+test_data = np.array([12, 0])  # (예습, 복습) = (12, 0) => Pass (1)
+
+print(predict(test_data))
+
+x_0 = []
+y_0 = []
+x_1 = []
+y_1 = []
+for i, j in enumerate(t_data):
+    print(i, j)
+    if j == 0:
+        x_0.append(x_data[i][0])
+        y_0.append(x_data[i][1])
+    elif j == 1:
+        x_1.append(x_data[i][0])
+        y_1.append(x_data[i][1])
+
+
+plt.scatter(x_0, y_0, color='red')
+plt.scatter(x_1, y_1, color='blue')
+plt.show()
